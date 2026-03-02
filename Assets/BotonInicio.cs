2f0e8c6800f7d94a5c87fa62ec265990423f284b
IsaacOvalle <isaac.imanol@outlook.com>
2026-03-02 11:22:57 -0600

    cambios extra

---
+++ Assets/BotonInicio.cs
@@ -0,0 +1,19 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BotonInicio : MonoBehaviour
+{
+    public void Jugar()
+    {
+        Debug.Log("Iniciando");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+    }
+
+
+    public void Salir()
+    {
+        Application.Quit();
+
+    }
+}
